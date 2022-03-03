b39e766f5a32e068ac214bcefd58010b72f0b8b3
immgcb <32258697+immgcb@users.noreply.github.com>
2022-03-03 15:06:25 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,9 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" style="background-color:#9df8ac">
-           <img src="https://p7.hiclipart.com/preview/811/817/139/jenkins-continuous-integration-build-automation-continuous-delivery-software-build-integration-thumbnail.jpg" width="50" height="100"> <p>
-                </div>
-                </div>
+  
             <h1>CI CD (Jenkins) Live Demo Test # 12</h1> 
    
         </div>
