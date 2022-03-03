957847c88c144f9c01feb24304a75029edaa4a71
immgcb <32258697+immgcb@users.noreply.github.com>
2022-03-03 15:13:18 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>CI CD Test</title>
+        <title>CI CD Live Demo</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
