6a33546bcae9fcb341c162be729e566e46a0fb78
immgcb <32258697+immgcb@users.noreply.github.com>
2022-08-23 16:44:33 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" style="background-color:#9df8ac">
   
-            <h1>CI CD (Jenkins) Live Demo Test 1242152155</h1> 
+            <h1>CI CD (Jenkins) Live Demo Test 88888888888888888</h1> 
    
         </div>
     </body>
